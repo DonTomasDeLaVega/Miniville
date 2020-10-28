aeabaa3da7eeb235c81ef7355f61d736ba42a12e
DonTomasDeLaVega <soriku@outlook.com>
2020-10-28 10:50:39 +0100

    Projet entier, quasi vide

---
+++ Cards.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniVille
+{
+    class Cards
+    {
+        public int AcVal; //Valeur d'activation
+        public string Color;
+        public string name;
+        public int nbPieceRecue;
+        public int Cout; //Cout de la carte
+        public bool vol;
+        public Player Launcher;
+        public Cards(int val, string col, string name, int nbPiece, int cout,Player launcher)
+        {
+            this.AcVal = val;
+            this.Color = col;
+            this.name = name;
+            this.nbPieceRecue = nbPiece;
+            this.Cout = cout;
+            this.Launcher = launcher;
+            if (Color == "red") vol = true;
+            //Trouver le joueur propriétaire de la carte construite. Comment?
+        }
+        public override string ToString()
+        {
+            string toString = string.Format("+----+\n|{0}   |\n|    |\n|    |\n|{1}  {2}|\n+-----+", AcVal, nbPieceRecue, Cout);
+            //Penser à changer la couleur du cw
+            return toString;
+        }
+    }
+}
