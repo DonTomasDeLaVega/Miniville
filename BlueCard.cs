56c8fd934f2e4335abe337700b1befcc9496d942
Thomas <thomasciampossin@live.fr>
2020-10-29 12:01:33 +0100

    Ajoute de la classe blue card

---
+++ BlueCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniVille
+{
+    class BlueCard : Cards
+    {
+        public int AcVal; //Valeur d'activation
+        public string Color;
+        public string name;
+        public int nbPieceRecue;
+        public int Cout; //Cout de la carte
+        public BlueCard() : base(val, name, nbPiece, cout)
+        {
+            Color = "blue";
+            AcVal = val;
+            this.name = name;
+            nbPieceRecue = nbPiece;
+
+        }
+    }
+}
